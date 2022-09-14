2352f99e961dc6f95cc9b913bdd1e1003b343d6a
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-09-14 15:15:53 +0430

    Add test to the AC button

--- src/logic/_tests_/calculate.test.js
+++ src/logic/_tests_/calculate.test.js
@@ -1,1 +1,13 @@
 import calculate from '../calculate';
+
+test('Test the functionality for AC button', () => {
+  const obj = {
+    total: 10,
+    next: 20,
+    operation: '+',
+  };
+  const result = calculate(obj, 'AC');
+  expect(result.total).toBe(null);
+  expect(result.next).toBe(null);
+  expect(result.operation).toBe(null);
+});
