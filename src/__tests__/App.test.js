afb1d6c78ce31ae17395c6cad46f83ce4b8e42c4
David-Kasilia <davidkasilia001@gmail.com>
2022-09-15 12:01:37 +0300

    Fix eslinters errors

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom';
 import App from '../App';
 
 test('Unit Test Check For App Component', () => {
-  render(<BrowserRouter><App /></BrowserRouter>)
+  render(<BrowserRouter><App /></BrowserRouter>);
   const linkElement = screen.getByText(/Welcome To Our Page/i);
   expect(linkElement).toBeInTheDocument();
 });
