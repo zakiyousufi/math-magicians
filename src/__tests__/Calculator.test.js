3e4c7c928d267506884f3cf786004c84accd0b30
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2023-11-01 14:50:42 +0100

    Delete all file so I can create New files

--- src/__tests__/Calculator.test.js
+++
@@ -1,9 +0,0 @@
-import TestRenderer from 'react-test-renderer';
-import Calculator from '../components/pages/Calculator';
-
-describe('Unit Test For Calculator component', () => {
-  it('Calculator Component renders correctly', () => {
-    const tree = TestRenderer.create(<Calculator />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
